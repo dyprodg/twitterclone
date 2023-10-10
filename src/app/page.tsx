e246740cf3f309388ce87d68a79d3ad8765bc2d6
dyprodg <dennisdiepolder@outlook.com>
2023-10-10 11:41:26 +0200

    timeline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import LeftSidebar from '@/components/LeftSidebar';
 import Link from 'next/link';
+import { BsChat, BsDot, BsThreeDots } from 'react-icons/bs';
+import { AiOutlineHeart, AiOutlineRetweet} from 'react-icons/ai';
+import { IoStatsChart, IoShareOutline } from 'react-icons/io5';
 
 
 export default function Home() {
@@ -11,8 +14,8 @@ export default function Home() {
 
 
         {/* Main Section for Blog*/}
-        <main className='ml-[320px] flex w-[700px] h-full min-h-screen flex-col border-l-[0.5px] border-r border-gray-600'>
-          <h1 className='text-2xl font-bold px-4 py-2 my-4'>Home</h1>
+        <main className='ml-[320px] flex w-full max-w-[700px] h-full min-h-screen flex-col border-l-[0.5px] border-r border-gray-600'>
+          <h1 className='text-2xl font-bold px-4 py-2 my-4 backdrop-blur bg-black/10 sticky top-0 z-20'>Home</h1>
           <div className='border-t-[0.5px] border-b-[0.5px] px-4 flex items-stretch space-x-2 py-4 border-gray-600 relative'>
             <div className='w-10 h-10 bg-slate-400 rounded-full flex-none'></div>
             <div className='flex flex-col w-full'>
@@ -29,6 +32,40 @@ export default function Home() {
                 </div>
               </div>
             </div>
+          </div>
+
+          <div className='flex flex-col'>
+            {
+              Array.from({length:5}).map((_,i)=> (
+                <div key={i} className=' py-4 px-6 border-b-[0.5px] border-gray-600 flex space-x-4'>
+                  <div>
+                    <div className='w-10 h-10 bg-slate-200 rounded-full'/>
+                  </div>
+                  <div className='flex flex-col'>
+                    <div className='flex items-center justify-between w-full'>
+                      <div className='flex items-center space-x-1 w-full'>
+                        <div className='font-bold'>Dennis Diepolder</div>
+                        <div className='text-gray-500'>@dyprodg</div>
+                        <div className='text-gray-500'> <BsDot /></div>
+                        <div className='text-gray-500'>1 hour ago</div>
+                      </div>
+                      <div><BsThreeDots /></div>
+                    </div>
+                    <div className='text-white text-lg mt-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A et animi quae veniam odio expedita tempore ratione reprehenderit neque suscipit? Ex totam culpa fuga voluptates commodi fugiat excepturi nesciunt vel expedita? Veritatis perferendis dignissimos iste veniam reprehenderit quaerat delectus ea.</div>
+                    <div className='bg-slate-400 aspect-square w-full h-96 rounded-xl mt-3'>
+                      Media
+                    </div>
+                    <div className='flex items-center justify-evenly w-full mt-4'>
+                      <div className='rounded-full hover:bg-white/20 transition duration-100 p-2 curser-pointer'><BsChat /></div>
+                      <div className='rounded-full hover:bg-white/20 transition duration-100 p-2 curser-pointer'><AiOutlineRetweet /></div>
+                      <div className='rounded-full hover:bg-white/20 transition duration-100 p-2 curser-pointer'><AiOutlineHeart /></div>
+                      <div className='rounded-full hover:bg-white/20 transition duration-100 p-2 curser-pointer'><IoStatsChart /></div>
+                      <div className='rounded-full hover:bg-white/20 transition duration-100 p-2 curser-pointer'><IoShareOutline /></div>
+                    </div>
+                  </div>
+                </div>
+              ))
+            }
           </div>
         </main>
         {/* Right Section for Recommandtations and Trendings */}
