e6bcb716bb1ea14efc60b7aac7c97419b6aeb8f1
dyprodg <dennisdiepolder@outlook.com>
2023-10-11 13:38:54 +0200

    leftside

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,76 +1,87 @@
 import LeftSidebar from '@/components/LeftSidebar';
-import Link from 'next/link';
-import { BsChat, BsDot, BsThreeDots } from 'react-icons/bs';
-import { AiOutlineHeart, AiOutlineRetweet} from 'react-icons/ai';
-import { IoStatsChart, IoShareOutline } from 'react-icons/io5';
+import MainComp from '@/components/MainComp';
+
+import { BsSearch } from 'react-icons/bs';
+
 
 
 export default function Home() {
   return (
     <div className='w-full h-full flex justify-center items-center relative bg-black'>
       <div className='max-w-screen-xl w-full h-full flex relative'>
-        {/* Left Sidebar Navbar */}
         <LeftSidebar />
+        <MainComp />
+        
 
 
-        {/* Main Section for Blog*/}
-        <main className='ml-[320px] flex w-full max-w-[700px] h-full min-h-screen flex-col border-l-[0.5px] border-r border-gray-600'>
-          <h1 className='text-2xl font-bold px-4 py-2 my-4 backdrop-blur bg-black/10 sticky top-0 z-20'>Home</h1>
-          <div className='border-t-[0.5px] border-b-[0.5px] px-4 flex items-stretch space-x-2 py-4 border-gray-600 relative'>
-            <div className='w-10 h-10 bg-slate-400 rounded-full flex-none'></div>
-            <div className='flex flex-col w-full'>
-              <input 
-                type='text' 
-                placeholder="What's happening" 
-                className='w-full h-full bg-transparent py-4 border-b-[0.5px] border-gray-600 outline-none border-none placeholder:text-2xl '/>
-              <div className='w-full justify-between items-center flex'>
-                <div>
 
-                </div>
-                <div className='w-full max-w-[100px]'>
-                  <button className='rounded-full bg-primary px-4 py-2 w-full text-lg text-center hover:bg-opacity-70 transition duration-200 font-bold'>Tweet</button>
-                </div>
-              </div>
+        <section className='fixed right-0 flex flex-col w-[400px] border items-stretch h-screen px-6'>
+          <div className='mt-2'>
+            <div className='relative w-full h-full group' >
+            <input 
+              id='searchBox' 
+              type='text' 
+              placeholder='Search Twitter' 
+              className='outline-none peer focus:border-primary border border-black placeholder:px-2 bg-neutral-800/90 w-full h-full rounded-full py-4 px-8' />
+            <label htmlFor='searchBox' className='absolute top-0 left-0 h-full flex items-center justify-center px-3 text-gray-500 peer-focus:text-primary'>
+              <BsSearch className='w-5 h-5' />
+            </label>
             </div>
           </div>
 
-          <div className='flex flex-col'>
-            {
-              Array.from({length:5}).map((_,i)=> (
-                <div key={i} className=' py-4 px-6 border-b-[0.5px] border-gray-600 flex space-x-4'>
-                  <div>
-                    <div className='w-10 h-10 bg-slate-200 rounded-full'/>
+          <div className='flex flex-col rounded-xl bg-neutral-800 my-3'>
+            <h3 className='font-bold text-xl my-2 py-2 px-4'>What's happening</h3>
+            <div>
+              {
+                Array.from({length: 5}).map((_,i)=>(
+                  <div key={i} className='hover:bg-white/10 transition duration-100 p-4'>
+                    <div className='font-bold text-lg'>#trending ${i+1}</div>
+                    <div className='text-xs text-neutral-400'>35.5k</div>
                   </div>
-                  <div className='flex flex-col'>
-                    <div className='flex items-center justify-between w-full'>
-                      <div className='flex items-center space-x-1 w-full'>
-                        <div className='font-bold'>Dennis Diepolder</div>
-                        <div className='text-gray-500'>@dyprodg</div>
-                        <div className='text-gray-500'> <BsDot /></div>
-                        <div className='text-gray-500'>1 hour ago</div>
-                      </div>
-                      <div><BsThreeDots /></div>
-                    </div>
-                    <div className='text-white text-lg mt-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A et animi quae veniam odio expedita tempore ratione reprehenderit neque suscipit? Ex totam culpa fuga voluptates commodi fugiat excepturi nesciunt vel expedita? Veritatis perferendis dignissimos iste veniam reprehenderit quaerat delectus ea.</div>
-                    <div className='bg-slate-400 aspect-square w-full h-96 rounded-xl mt-3'>
-                      Media
-                    </div>
-                    <div className='flex items-center justify-evenly w-full mt-4'>
-                      <div className='rounded-full hover:bg-white/20 transition duration-100 p-2 curser-pointer'><BsChat /></div>
-                      <div className='rounded-full hover:bg-white/20 transition duration-100 p-2 curser-pointer'><AiOutlineRetweet /></div>
-                      <div className='rounded-full hover:bg-white/20 transition duration-100 p-2 curser-pointer'><AiOutlineHeart /></div>
-                      <div className='rounded-full hover:bg-white/20 transition duration-100 p-2 curser-pointer'><IoStatsChart /></div>
-                      <div className='rounded-full hover:bg-white/20 transition duration-100 p-2 curser-pointer'><IoShareOutline /></div>
-                    </div>
-                  </div>
-                </div>
-              ))
-            }
+                ))
+              }
+            </div>
           </div>
-        </main>
-        {/* Right Section for Recommandtations and Trendings */}
-        <section></section>
+
+
+          <div className='flex flex-col rounded-xl bg-neutral-800 my-3'>
+            <h3 className='font-bold text-xl my-2 py-2 px-4'>Who to follow</h3>
+            <div>
+              {
+                Array.from({length: 3}).map((_,i)=>(
+                  <div className='flex justify-between items-center'>
+                    <div key={i} className='flex hover:bg-white/10 transition duration-100 p-4 space-x-3 items-center'>
+                      <div className='w-10 h-10 bg-neutral-600 rounded-full'></div>
+                      <div className='flex flex-col'>
+                      <div className='font-bold text-white'>Other User</div>
+                      <div className='text-neutral-400'>@otheruser1234</div>
+                  </div>
+                  </div>
+                  
+
+                    <div>
+                      <button className='rounded-full px-6 py-2 mx-4 bg-white text-black'>Follow</button>
+                    </div>
+                  </div>
+                ))
+              }
+            </div>
+          </div>
+
+
+          
+
+
+
+
+
+
+
+
+          <div></div>
+        </section>
       </div>
     </div>
   )
 }
+ 
